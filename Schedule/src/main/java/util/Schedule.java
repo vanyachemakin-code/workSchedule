24ca185fe5ce24749890dc5a10e7330d38bcd9a1
vanyachemakin <work.vanyachemakin@gmail.com>
2025-09-07 00:53:39 +0300

    upd

--- Schedule/src/main/java/util/Schedule.java
+++ Schedule/src/main/java/util/Schedule.java
@@ -5,8 +5,6 @@ import dto.EmployeeDto;
 import dto.ScheduleDto;
 import lombok.*;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -15,22 +13,9 @@ import java.util.*;
 @Data
 public class Schedule {
 
-    private FileWriter writer;
     private final Map<LocalDate, Integer> employeeCountPerDay = new HashMap<>();
     private final Month month;
     private final CompanyDto companyDto;
-
-    public Schedule(Month month, CompanyDto companyDto) {
-        this.month = month;
-        this.companyDto = companyDto;
-        try {
-            writer = new FileWriter(
-                    "/Users/vanyachemakin/IdeaProjects/workSchedule/Schedule/schedule/schedule.txt"
-            );
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     private int getEmployeeMonthShifts() {
         return Math.round((float) HolidayChecker.getDaysInMonth(month) / companyDto.getEmployeeDtos().size());
@@ -48,17 +33,6 @@ public class Schedule {
         }
     }
 
-    private void scheduleWriter(String dayShifts) {
-        try {
-            writer.write(dayShifts);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    //доработать
     public List<ScheduleDto> generate() {
         setEmployeeMonthShifts();
         setEmployeeCountPerDay();
@@ -74,11 +48,11 @@ public class Schedule {
                 if (employee.getShiftsInARow() < 3 && employee.getMonthShifts() != 0) {
                     employeePerDay.add(employee.getName());
                 }
+                if (employee.getShiftsInARow() == 4) employee.setShiftsInARow(0);
                 if (employeePerDay.size() == employeeCountPerDay.get(day)) {
                     schedule.add(new ScheduleDto(day.format(DateTimeFormatter.ofPattern("dd/MM")), employeePerDay));
                     continue point;
                 }
-                if (employee.getShiftsInARow() == 4) employee.setShiftsInARow(0);
             }
         }
         return schedule;
